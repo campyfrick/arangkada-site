6fbbf920b5036ed4ecdd78b023d85f64ae1d7978
Francis Campos <fedric.campos@gmail.com>
2025-11-30 15:12:34 +0800

    changed to hash router

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,19 @@ import "@mantine/core/styles.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { MantineProvider } from "@mantine/core";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { mantineTheme } from "./theme";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <MantineProvider
         defaultColorScheme="light"
         theme={mantineTheme}
       >
         <App />
       </MantineProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
